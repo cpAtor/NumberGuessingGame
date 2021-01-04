fafbd8fd6ad8aa5bb03929a20094c2be418ce33a
Chaitanya.Pathipati <chaitanyakumar.pathipati@gmail.com>
2021-01-04 20:42:48 +0530

    random number code is added

--- 1_random_number.py
+++ 1_random_number.py
@@ -6,3 +6,25 @@ Created on Mon Jan  4 17:01:08 2021
 """
 
 # 1. write a function that returns a random number between 1 and 100
+# Python code to generate 
+# random numbers and 
+# append them to a list 
+import random 
+  
+# Function to generate 
+# and append them  
+# start = starting range, 
+# end = ending range 
+# num = number of  
+# elements needs to be appended 
+start= input("enter your start value: ")
+end= input("enter your end value: ")
+start=int(start)
+end=int(end)
+def Rand(start, end, ): 
+   
+    num=random.randint(start, end) 
+  
+    return num
+  
+print(Rand(start, end))
